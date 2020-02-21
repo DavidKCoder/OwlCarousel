c9a913a073b3fb958994a98125d8376e79d52cfa
DavidKizinger <karapetyand91@gmail.com>
2020-02-21 15:38:43 +0400

    refactor

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+    // Owl Carousel
+    let owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 3,
+      margin: 10,
+      loop: true,
+      nav: true
+    });
+  });
+
+  
